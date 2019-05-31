cb31e414be2af37e74092f21fd85527f73420f76
Lucas Youngers <lucasyoungers@Adams-MacBook-Pro-4.local>
2019-05-31 07:36:10 -0700

    done, no css yet

---
+++ src/views/Chat.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <h2>Orchid</h2>
+    <h5>A Vue.js and Firebase Powered Chat App</h5>
+    <div>
+      <div>
+        <p v-if="messages.length == 0">[No messages yet!]</p>
+        <div v-chat-scroll="{ always: false, smooth: true }">
+          <div v-for="message in messages" :key="message.id">
+            <span>[{{ message.name }}]:</span>
+            <span>{{ message.message }}</span>
+            <span>{{ message.timestamp }}</span>
+          </div>
+        </div>
+      </div>
+      <div>
+        <CreateMessage :name="name"/>
+      </div>
+    </div>
+  </div>
+</template>
---
+++ src/views/Login.vue
@@ -0,0 +1,42 @@
+<template>
+  <div>
+    <div>
+      <div>
+        <h2>Login</h2>
+        <form @submit.prevent="login">
+          <div>
+            <input
+              type="text"
+              class="form-control"
+              placeholder="Please enter your name ..."
+              name="name"
+              v-model="name"
+            >
+            <p v-if="errorText">{{ errorText }}</p>
+          </div>
+          <button>Enter Chat</button>
+        </form>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: "home",
+  data() {
+    return {
+      name: "",
+      errorText: null
+    };
+  },
+  methods: {
+    login() {
+      if (this.name) {
+        this.$router.push({ name: "Chat", params: { name: this.name } });
+      } else {
+        this.errorText = "Please enter a name!";
+      }
+    }
+  }
+};
+</script>
