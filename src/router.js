7c770b74c4ad03c139d9f6cd4306e1a3fb2d68f8
Lucas Youngers <lucasyoungers@Adams-MacBook-Pro-4.local>
2019-05-31 07:40:03 -0700

    updated router

--- src/router.js
+++ src/router.js
@@ -1,25 +1,32 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Home from './views/Home.vue'
+import Login from '@/src/Login.vue'
+import Chat from '@/src/Chat.vue'
 
 Vue.use(Router)
 
 export default new Router({
   mode: 'history',
   base: process.env.BASE_URL,
-  routes: [
-    {
+  routes: [{
       path: '/',
-      name: 'home',
-      component: Home
+      name: 'Login',
+      component: Login
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
+      path: '/chat',
+      name: 'Chat',
+      component: Chat,
+      props: true,
+      beforeEnter: (to, from, next) => {
+        if (to.params.name) {
+          next();
+        } else {
+          next({
+            name: 'Login'
+          });
+        }
+      }
     }
   ]
 })
